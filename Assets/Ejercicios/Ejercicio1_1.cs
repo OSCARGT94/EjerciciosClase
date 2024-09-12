276746d7752072b7787c3d65507331f946ea8709
OSCARGT94 <oscarfernan94@gmail.com>
2024-09-12 17:57:17 +0200

    Ejercicio1_1

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
+    string nombre = "oscar";
+    int frame;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("¡Hola mundo!");
+        Debug.Log( "Este es el primer videojuego de " + nombre );
+        Debug.Log("Estoy aprendiendo C# ");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Ha pasado " + frame + " frame/s");
+        frame++;
+        //Este mensaje se repite constatemente al pasar cada frame, pudiendo variar dependiendo de la velocidad del ordenador.
     }
 }
