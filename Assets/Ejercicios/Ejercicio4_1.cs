b2995091961f39988a6bb79618f365dbceda97fd
OSCARGT94 <oscarfernan94@gmail.com>
2024-09-16 21:24:03 +0200

    hasta el 4_1

---
+++ Assets/Ejercicios/Ejercicio4_1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio4_1 : MonoBehaviour
+{
+    float resultadoAreaCirculo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float resultadoAreacirculo = areacirculo();
+        Debug.Log("El resultado del area del circulo es ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    float areacirculo(float radio, float pi)
+    {
+        float resultadoArea = (radio * 2) * pi;
+        return resultadoArea;
+    }
+}
