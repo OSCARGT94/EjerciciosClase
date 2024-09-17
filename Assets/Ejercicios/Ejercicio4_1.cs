79caa9d0b4d6909b40f236a4d9cc3cddf26b95a5
Oscar <oscarfernan94@gmail.com>
2024-09-17 11:30:36 +0200

    Hasta 4_1 entero

--- Assets/Ejercicios/Ejercicio4_1.cs
+++ Assets/Ejercicios/Ejercicio4_1.cs
@@ -10,6 +10,12 @@ public class Ejercicio4_1 : MonoBehaviour
     {
         float resultadoAreacirculo = areacirculo(3,3.14f);
         Debug.Log("El resultado del area del circulo es " + resultadoAreacirculo);
+        
+        float resultadoAreaTriangulo = areatriangulo(3,3);
+        Debug.Log("El resultado del area del circulo es " + resultadoAreaTriangulo);
+        
+        float resultadoAreaCuadrado = areacuadrado(4);
+        Debug.Log("El resultado del area del circulo es " + resultadoAreaCuadrado);
     }
 
     // Update is called once per frame
@@ -19,7 +25,17 @@ public class Ejercicio4_1 : MonoBehaviour
     }
     float areacirculo(float radio, float pi)
     {
-        float resultadoArea = (radio * 2) * pi;
+        float resultadoArea = (radio * radio) * pi;
+        return resultadoArea;
+    }
+    float areatriangulo(float basee, float altura)
+    {
+        float resultadoArea = (basee * altura) / 2;
+        return resultadoArea;
+    }
+    float areacuadrado(float lado)
+    {
+        float resultadoArea = lado * lado;
         return resultadoArea;
     }
 }
