37d0bac4226884ec704ca367305d4bcef5aaad8a
Oscar <oscarfernan94@gmail.com>
2024-09-18 11:45:53 +0200

    Hasta el 4_3 hecho

--- Assets/Ejercicios/Ejercicio4_3.cs
+++ Assets/Ejercicios/Ejercicio4_3.cs
@@ -9,7 +9,8 @@ public class Ejercicio4_3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        convertir(cantidad,moneda);
+        string resultadoConversion = convertir(cantidad,moneda);
+        Debug.Log(resultadoConversion);
     }
 
     // Update is called once per frame
@@ -17,19 +18,24 @@ public class Ejercicio4_3 : MonoBehaviour
     {
         
     }
-    void convertir(float cantidad, string moneda)
+    string convertir(float cantidad, string moneda)
     {
         if (moneda == "euros")
         {
             float conversionADolar = cantidad * 1.10795f;
+            string conversion = cantidad + " euros equivalen a " + conversionADolar + " dolars.";
+            return conversion;
         }
         else if (moneda == "dolars")
         {
-
+            float conversionADolar = cantidad * 0.898012f;
+            string conversion = cantidad + " dolars equivalen a " + conversionADolar + " euros.";
+            return conversion;
         }
         else
         {
-
+            string conversion = "Porfavor, en moneda escribre euros o dolars.";
+            return conversion;
         }
     }
 }
