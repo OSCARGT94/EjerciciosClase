fba41647db7449ea59c131990f387c87eb5abceb
Oscar <oscarfernan94@gmail.com>
2024-09-24 12:02:52 +0200

    Hasta el 7_4

--- Assets/Ejercicios/Ejercicio7_3.cs
+++ Assets/Ejercicios/Ejercicio7_3.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio7_2 : MonoBehaviour
+public class Ejercicio7_3 : MonoBehaviour
 {
-    
+    int numeros = 100;
     // Start is called before the first frame update
     void Start()
     {
-        for (int numeros = 1; numeros < 101; numeros++)
+        while (numeros > 0)
         {
             Debug.Log(numeros);
+            numeros--;
         }
     }
 
--- Assets/Ejercicios/Ejercicio7_4.cs
+++ Assets/Ejercicios/Ejercicio7_4.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio7_2 : MonoBehaviour
+public class Ejercicio7_4 : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
-        for (int numeros = 1; numeros < 101; numeros++)
+        for (int numeros = 100; numeros > 0; numeros--)
         {
             Debug.Log(numeros);
         }
