4c1564f2741a6a02cdb0d453479ad350e69d26c7
Oscar <oscarfernan94@gmail.com>
2024-09-24 11:23:09 +0200

    Hasta el ejercicio 5_7

--- Assets/Ejercicios/Ejercicio5_7.cs
+++ Assets/Ejercicios/Ejercicio5_7.cs
@@ -8,7 +8,39 @@ public class Ejercicio5_7 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (velocidad1 > velocidad2 && velocidad1 > velocidad3)
+        {
+            if (velocidad2 > velocidad3)
+            {
+                Debug.Log("El orden de los turnos es el siguiente, primero el jugador 1, luego jugador 2 y luego jugador 3.");
+            }
+            else
+            {
+                Debug.Log("El orden de los turnos es el siguiente, primero el jugador 1, luego jugador 3 y luego jugador 2.");
+            }
+        }
+        else if (velocidad2 > velocidad1 && velocidad2 > velocidad3)
+        {
+            if (velocidad1 > velocidad3)
+            {
+                Debug.Log("El orden de los turnos es el siguiente, primero el jugador 2, luego jugador 1 y luego jugador 3.");
+            }
+            else
+            {
+                Debug.Log("El orden de los turnos es el siguiente, primero el jugador 2, luego jugador 3 y luego jugador 1.");
+            }
+        }
+        else
+        {
+            if (velocidad1 > velocidad2)
+            {
+                Debug.Log("El orden de los turnos es el siguiente, primero el jugador 3, luego jugador 1 y luego jugador 2.");
+            }
+            else
+            {
+                Debug.Log("El orden de los turnos es el siguiente, primero el jugador 3, luego jugador 2 y luego jugador 1.");
+            }
+        }
     }
 
     // Update is called once per frame
